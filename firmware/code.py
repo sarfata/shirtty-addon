bdf1f7ff13ddb759b576b6a4a994b4cadf6cdc62
Chris Gervang <chris@gervang.com>
2019-11-15 17:25:40 -0800

    Updating Friday night fixes. Power remote to turn off!

--- firmware/code.py
+++ firmware/code.py
@@ -12,14 +12,15 @@ red = pulseio.PWMOut(board.D5, frequency=38000, duty_cycle=0)
 green = pulseio.PWMOut(board.D6, frequency=38000, duty_cycle=0)
 blue = pulseio.PWMOut(board.D7, frequency=38000, duty_cycle=0)
 
-irtx = pulseio.PWMOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
-irout = pulseio.PulseOut(irtx)
+# No IR Out without an extra capacitor on the power supply
+# irtx = pulseio.PWMOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
+# irout = pulseio.PulseOut(irtx)
 
 # Measuring IR pulses
 pulsein = pulseio.PulseIn(board.IR_RX, maxlen=200, idle_state=True)
 
-encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500], one=[550, 550],
-                                            zero=[550, 1700], trail=0)
+# encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500], one=[550, 550],
+#                                             zero=[550, 1700], trail=0)
 decoder = adafruit_irremote.GenericDecode()
 
 
@@ -50,6 +51,33 @@ def setColorHSL(h, s, l):
     green.duty_cycle = math.floor((2**16-1) * (1-rgb[1]))
     blue.duty_cycle = math.floor((2**16-1) * (1-rgb[2]))
 
+
+
+def matchNerfPulses(pulses):
+    if len(pulses) != 17:
+        print("wrong number of pulses {}".format(len(pulses)))
+        return None
+
+    reference1 = [2957, 5996, 2974, 2009, 991, 1987, 987, 2008, 992, 1987, 983, 2008, 1962, 2012, 987, 2008, 966]
+    reference2 = [2935, 6037, 2957, 2038, 940, 2047, 961, 2039, 914, 2073, 1957, 2042, 910, 2094, 940, 2042, 936]
+    
+
+    def matchAgainstKnownPulse(pulses, knownReference):
+        for i in range(0, 17, 1):
+            if math.fabs(pulses[i] - knownReference[i]) > 150:
+                print("pulse {} does not match {} vs {}".format(i, pulses[i], knownReference[i]))
+                return None
+        return True
+
+    if matchAgainstKnownPulse(pulses, reference1):
+        print("Nerf Blast Team1")
+        return 1
+    if matchAgainstKnownPulse(pulses, reference2):
+        print("Nerf Blast Team2")
+        return 2
+    return None
+
+
 def doTheColorRampDance(hue, duration, upAndDown = False, count = 1):
     if upAndDown:
         duration = duration / 2
@@ -68,76 +96,61 @@ def doTheColorRampDance(hue, duration, upAndDown = False, count = 1):
 
         count = count - 1
 
-def matchNerfPulses(pulses):
-    if len(pulses) != 17:
-        print("wrong number of pulses {}".format(len(pulses)))
-        return None
+rgbOff()
+# [0, 6[
+currentHue = 0
+# 0 to 100
+currentIntensity = 0
+speed = 1
 
-    reference = [2935, 6037, 2957, 2038, 940, 2047, 961, 2039, 914, 2073, 1957, 2042, 910, 2094, 940, 2042, 936]
-    for i in range(0, 17, 1):
-        if math.fabs(pulses[i] - reference[i]) > 150:
-            print("pulse {} does not match {} vs {}".format(i, pulses[i], reference[i]))
-            return None
-
-    return True
-
+power = True
 
 while True:
-    print("Looping!")
+    if power:
+        currentIntensity = currentIntensity + speed
 
-    rgbOff()
+        if currentIntensity >= 100:
+            speed = speed * -1
 
-    # empty pulse buffer so we do not react to last message we sent
+        if currentIntensity <= 0:
+            speed = speed * -1
+            currentHue = (currentHue + 0.3) % 6
+
+        setColorHSL(currentHue, 1, currentIntensity / 200)
+    else:
+        rgbOff()
+
+
+
     pulses = decoder.read_pulses(pulsein, blocking= False)
+    if pulses:
+        print("Heard", len(pulses), "Pulses:", pulses)
 
-    # Pick a random hue
-    hue = random.random() * 6
+        nerfBlast = matchNerfPulses(pulses)
+        if nerfBlast == 1:
+            setColorHSL(1, 1, 0.5)
+            time.sleep(2)
+        elif nerfBlast == 2:
+            setColorHSL(3, 1, 0.5)
+            time.sleep(2)
 
-    # sleep for random number of sec.
-    sleepUntil = time.monotonic() + random.randint(3, 10)
-    while time.monotonic() < sleepUntil:
-        pulses = decoder.read_pulses(pulsein, blocking= False)
-        if pulses:
-            print("Heard", len(pulses), "Pulses:", pulses)
-
-            if matchNerfPulses(pulses):
-                print("We have been blasted!!")
-                for hue in range(0, 6, 2):
-                    doTheColorRampDance(hue, 0.5, True)
-            else:
-                print("not a nerf blast")
-
-
+        else:
+            # Not a nerf blast - maybe a IR command
             try:
                 code = decoder.decode_bits(pulses)
                 print("Decoded:", code)
                 if len(code) == 5 and code[0:4] == [0xde, 0xad, 0xbe, 0xef]:
                     print("Got command: {}".format(code[4]))
                     doTheColorRampDance(code[4] * 6.0 / 255, 2, True, 5)
+                
+                if code == [124, 93]:
+                    print("Power!")
+                    power = not power
+                
             except adafruit_irremote.IRNECRepeatException:  # unusual short code!
                 print("NEC repeat!")
             except adafruit_irremote.IRDecodeException as e:     # failed to decode
                 print("Failed to decode: ", e.args)
 
-        # Check for i2c command
-        r = i2c_slave.request()
-        if r:
-            with r:  # Closes the transfer if necessary by sending a NACK or feeding the master dummy bytes
-                print("got i2c request! addr={} ".format(repr(r)))
-                if r.address == 0x42:
-                    if not r.is_read:  # Master write which is Slave read
-                        b = r.read(1)
-
-                        print("read bytes: {}".format(b))
-                        if (len(b) == 1):
-                            doTheColorRampDance(b[0] * 6.0 / 255, 2, True, 1)
-
-
-
-
-    doTheColorRampDance(hue, 4, False)
-
-    encoder.transmit(irout, [0xde, 0xad, 0xbe, 0xef, round(hue / 6 * 255)])
-    doTheColorRampDance(hue, 2, True, 5)
-
-
+# def matchCode(code, reference):
+#    if len(code) == len(reference) and code
