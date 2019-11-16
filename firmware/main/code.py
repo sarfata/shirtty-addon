2e09f2ab28ea08fc407e1c2f9288d58aae889c66
Chris Gervang <chris@gervang.com>
2019-11-15 18:03:29 -0800

    color library!

--- firmware/main/code.py
+++ firmware/main/code.py
@@ -7,10 +7,7 @@ import random
 import math
 import board
 from i2cslave import I2CSlave
-
-red = pulseio.PWMOut(board.D5, frequency=38000, duty_cycle=0)
-green = pulseio.PWMOut(board.D6, frequency=38000, duty_cycle=0)
-blue = pulseio.PWMOut(board.D7, frequency=38000, duty_cycle=0)
+from lib.color import rgb_off, set_color_hsl, do_the_color_ramp_dance
 
 # No IR Out without an extra capacitor on the power supply
 # irtx = pulseio.PWMOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
@@ -27,111 +24,64 @@ decoder = adafruit_irremote.GenericDecode()
 # i2c.writeto(0x42, bytes([0x42]), stop=False)
 i2c_slave = I2CSlave(board.SCL, board.SDA, (0x42, 0x44))
 
-
-def rgbOff():
-    red.duty_cycle = 0xffff
-    green.duty_cycle = 0xffff
-    blue.duty_cycle = 0xffff
-
-# https://codegolf.stackexchange.com/questions/150250/hsl-to-rgb-values
-# input h=[0,6[ s=[0,1] L=[0,1]
-def hsl2rgb(h,s,l):
-    c=(1-abs(2*l-1))*s;
-    m=l-c/2;
-    x=c*(1-abs(h%2-1))+m;
-    c+=m;
-    return[c,m,x,c,m,x,m,c,x,m][7-int(h)*5%9:][:3]
-
-def setColorHSL(h, s, l):
-    rgb = hsl2rgb(h, s, l)
-
-    #print(h, s, l, rgb)
-
-    red.duty_cycle = math.floor((2**16-1) * (1-rgb[0]))
-    green.duty_cycle = math.floor((2**16-1) * (1-rgb[1]))
-    blue.duty_cycle = math.floor((2**16-1) * (1-rgb[2]))
-
-
-
-def matchNerfPulses(pulses):
+def match_nerf_pulses(pulses):
     if len(pulses) != 17:
         print("wrong number of pulses {}".format(len(pulses)))
         return None
 
-    reference1 = [2957, 5996, 2974, 2009, 991, 1987, 987, 2008, 992, 1987, 983, 2008, 1962, 2012, 987, 2008, 966]
-    reference2 = [2935, 6037, 2957, 2038, 940, 2047, 961, 2039, 914, 2073, 1957, 2042, 910, 2094, 940, 2042, 936]
-    
+    team_1 = [2957, 5996, 2974, 2009, 991, 1987, 987, 2008, 992, 1987, 983, 2008, 1962, 2012, 987, 2008, 966]
+    team_2 = [2935, 6037, 2957, 2038, 940, 2047, 961, 2039, 914, 2073, 1957, 2042, 910, 2094, 940, 2042, 936]
 
-    def matchAgainstKnownPulse(pulses, knownReference):
+
+    def match_against_known_pulse(pulses, knownReference):
         for i in range(0, 17, 1):
             if math.fabs(pulses[i] - knownReference[i]) > 150:
                 print("pulse {} does not match {} vs {}".format(i, pulses[i], knownReference[i]))
                 return None
         return True
 
-    if matchAgainstKnownPulse(pulses, reference1):
+    if match_against_known_pulse(pulses, team_1):
         print("Nerf Blast Team1")
         return 1
-    if matchAgainstKnownPulse(pulses, reference2):
+    if match_against_known_pulse(pulses, team_2):
         print("Nerf Blast Team2")
         return 2
     return None
 
-
-def doTheColorRampDance(hue, duration, upAndDown = False, count = 1):
-    if upAndDown:
-        duration = duration / 2
-
-    while count > 0:
-        for i in range(0, 100, 1):
-            # we only go up to 50% lightness because it's white afterwards.
-            setColorHSL(hue, 1, i/2/100)
-            time.sleep(duration / 100)
-
-        if upAndDown:
-            for i in range(100, 0, -1):
-                # we only go up to 50% lightness because it's white afterwards.
-                setColorHSL(hue, 1, i/2/100)
-                time.sleep(duration / 100)
-
-        count = count - 1
-
-rgbOff()
+rgb_off()
 # [0, 6[
-currentHue = 0
+current_hue = 0
 # 0 to 100
-currentIntensity = 0
+current_intensity = 0
 speed = 1
 
 power = True
 
 while True:
     if power:
-        currentIntensity = currentIntensity + speed
+        current_intensity = current_intensity + speed
 
-        if currentIntensity >= 100:
+        if current_intensity >= 100:
             speed = speed * -1
 
-        if currentIntensity <= 0:
+        if current_intensity <= 0:
             speed = speed * -1
-            currentHue = (currentHue + 0.3) % 6
+            current_hue = (current_hue + 0.3) % 6
 
-        setColorHSL(currentHue, 1, currentIntensity / 200)
+        set_color_hsl(current_hue, 1, current_intensity / 200)
     else:
-        rgbOff()
-
-
+        rgb_off()
 
     pulses = decoder.read_pulses(pulsein, blocking= False)
     if pulses:
         print("Heard", len(pulses), "Pulses:", pulses)
 
-        nerfBlast = matchNerfPulses(pulses)
+        nerfBlast = match_nerf_pulses(pulses)
         if nerfBlast == 1:
-            setColorHSL(1, 1, 0.5)
+            set_color_hsl(1, 1, 0.5)
             time.sleep(2)
         elif nerfBlast == 2:
-            setColorHSL(3, 1, 0.5)
+            set_color_hsl(3, 1, 0.5)
             time.sleep(2)
 
         else:
@@ -141,16 +91,13 @@ while True:
                 print("Decoded:", code)
                 if len(code) == 5 and code[0:4] == [0xde, 0xad, 0xbe, 0xef]:
                     print("Got command: {}".format(code[4]))
-                    doTheColorRampDance(code[4] * 6.0 / 255, 2, True, 5)
-                
+                    do_the_color_ramp_dance(code[4] * 6.0 / 255, 2, True, 5)
+
                 if code == [124, 93]:
                     print("Power!")
                     power = not power
-                
+
             except adafruit_irremote.IRNECRepeatException:  # unusual short code!
                 print("NEC repeat!")
             except adafruit_irremote.IRDecodeException as e:     # failed to decode
                 print("Failed to decode: ", e.args)
-
-# def matchCode(code, reference):
-#    if len(code) == len(reference) and code
