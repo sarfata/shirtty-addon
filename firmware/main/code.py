c7417841149db91d76aef193b46ece64d0f7992f
Chris Gervang <chris@gervang.com>
2019-11-16 15:53:13 -0800

    ShittyColor class

--- firmware/main/code.py
+++ firmware/main/code.py
@@ -6,16 +6,15 @@ import random
 import math
 import board
 from i2cslave import I2CSlave
-from color import rgb_off, set_color_hsl, do_the_color_ramp_dance, hsl_to_rgb
+from color import ShittyRgb
 from ir.pulses import Pulses
 from ir.codes import Codes
 from ir import ShittyIr
 from pixels import ShittyPixels
 
 # i2c.writeto(0x42, bytes([0x42]), stop=False)
-i2c_slave = I2CSlave(board.SCL, board.SDA, (0x42, 0x44))
+# i2c_slave = I2CSlave(board.SCL, board.SDA, (0x42, 0x44))
 
-rgb_off()
 # [0, 6[
 current_hue = 0
 # 0 to 100
@@ -26,6 +25,9 @@ power = True
 
 shit_ir = ShittyIr()
 shit_pixels = ShittyPixels()
+# shit_pixels.on()
+shit_rgb = ShittyRgb()
+# shit_rgb.on()
 
 pixels_start_hue = 0
 
@@ -33,11 +35,11 @@ def handle_pulses(pulses):
     pulse_match = Pulses.match(pulses)
     if pulse_match is not None:
         if pulse_match == Pulses.nerf.NERF_TEAM_1:
-            set_color_hsl(1, 1, 0.5)
+            shit_rgb.set_color_hsl(1, 1, 0.5)
             time.sleep(2)
             return
         elif pulse_match == Pulses.nerf.NERF_TEAM_2:
-            set_color_hsl(3, 1, 0.5)
+            shit_rgb.set_color_hsl(3, 1, 0.5)
             time.sleep(2)
             return
     # Not a nerf blast - maybe a IR command
@@ -54,7 +56,7 @@ def handle_pulses(pulses):
             shit_pixels.toggle()
         elif code_match == Codes.shirtty.COLOR_SYNC:
             print("Color Sync! Got command: {}".format(code[4]))
-            do_the_color_ramp_dance(code[4] * 6.0 / 255, 2, True, 5)
+            shit_rgb.do_the_color_ramp_dance(code[4] * 6.0 / 255, 2, True, 5)
         elif code_match == Codes.sharp.POWER:
             print("Power!")
             power = not power
@@ -62,29 +64,30 @@ def handle_pulses(pulses):
 
 while True:
     # builtin RGB
-    if power:
-        current_intensity = current_intensity + speed
-        if current_intensity >= 100:
-            speed = speed * -1
-        if current_intensity <= 0:
-            speed = speed * -1
-            current_hue = (current_hue + 0.3) % 6
-        set_color_hsl(current_hue, 1, current_intensity / 200)
-    else:
-        rgb_off()
+    if shit_rgb.is_on:
+        if power:
+            current_intensity = current_intensity + speed
+            if current_intensity >= 100:
+                speed = speed * -1
+            if current_intensity <= 0:
+                speed = speed * -1
+                current_hue = (current_hue + 0.3) % 6
+            shit_rgb.set_color_hsl(current_hue, 1, current_intensity / 200)
+        else:
+            shit_rgb.rgb_off()
 
     # pixels
-    if shit_pixels.is_on():
-        for i in range(0, 64, 1):
-            row = round(i / 8)
-            column = i % 8
-            hue = (pixels_start_hue + row / 3 + column / 5) % 6
+    # if shit_pixels.is_on():
+    #     for i in range(0, 64, 1):
+    #         row = round(i / 8)
+    #         column = i % 8
+    #         hue = (pixels_start_hue + row / 3 + column / 5) % 6
 
-            rgb = hsl_to_rgb(hue, 1, 0.02)
-            shit_pixels.pixels[i] = (round(rgb[0]*255), round(rgb[1]*255), round(rgb[2]*255))
+    #         rgb = hsl_to_rgb(hue, 1, 0.02)
+    #         shit_pixels.pixels[i] = (round(rgb[0]*255), round(rgb[1]*255), round(rgb[2]*255))
 
-        shit_pixels.pixels.show()
-        pixels_start_hue = pixels_start_hue + .3
+    #     shit_pixels.pixels.show()
+    #     pixels_start_hue = pixels_start_hue + .3
 
     pulses = shit_ir.rx.get_pulses()
     handle_pulses(pulses)
--- firmware/main/lib/color.py
+++ firmware/main/lib/color.py
@@ -2,9 +2,6 @@ import board
 import pulseio
 import math
 import time
-red = pulseio.PWMOut(board.D5, frequency=38000, duty_cycle=0)
-green = pulseio.PWMOut(board.D6, frequency=38000, duty_cycle=0)
-blue = pulseio.PWMOut(board.D7, frequency=38000, duty_cycle=0)
 # https://codegolf.stackexchange.com/questions/150250/hsl-to-rgb-values
 # input h=[0,6[ s=[0,1] L=[0,1]
 def hsl_to_rgb(h,s,l):
@@ -13,26 +10,50 @@ def hsl_to_rgb(h,s,l):
     x=c*(1-abs(h%2-1))+m
     c+=m
     return[c,m,x,c,m,x,m,c,x,m][7-int(h)*5%9:][:3]
-def rgb_off():
-    red.duty_cycle = 0xffff
-    green.duty_cycle = 0xffff
-    blue.duty_cycle = 0xffff
-def set_color_hsl(h, s, l):
-    rgb = hsl_to_rgb(h, s, l)
-    red.duty_cycle = math.floor((2**16-1) * (1-rgb[0]))
-    green.duty_cycle = math.floor((2**16-1) * (1-rgb[1]))
-    blue.duty_cycle = math.floor((2**16-1) * (1-rgb[2]))
-def do_the_color_ramp_dance(hue, duration, up_and_down = False, count = 1):
-    if up_and_down:
-        duration = duration / 2
-    while count > 0:
-        for i in range(0, 100, 1):
-            # we only go up to 50% lightness because it's white afterwards.
-            set_color_hsl(hue, 1, i/2/100)
-            time.sleep(duration / 100)
-        if up_and_down:
-            for i in range(100, 0, -1):
-                # we only go up to 50% lightness because it's white afterwards.
-                set_color_hsl(hue, 1, i/2/100)
-                time.sleep(duration / 100)
-        count = count - 1
+
+class ShittyRgb(object):
+    def __init__(self):
+        self.red = None
+        self.green = None
+        self.blue = None
+        self.is_on = False
+    def on(self):
+        self.red = pulseio.PWMOut(board.D5, frequency=38000, duty_cycle=0)
+        self.green = pulseio.PWMOut(board.D6, frequency=38000, duty_cycle=0)
+        self.blue = pulseio.PWMOut(board.D7, frequency=38000, duty_cycle=0)
+        self.rgb_off()
+        self.is_on = True
+    def off(self):
+        self.red.deinit()
+        self.green.deinit()
+        self.blue.deinit()
+        self.red = None
+        self.green = None
+        self.blue = None
+        self.is_on = False
+    def rgb_off(self):
+        if self.is_on:
+            self.red.duty_cycle = 0xffff
+            self.green.duty_cycle = 0xffff
+            self.blue.duty_cycle = 0xffff
+    def set_color_hsl(self, h, s, l):
+        if self.is_on:
+            rgb = hsl_to_rgb(h, s, l)
+            self.red.duty_cycle = math.floor((2**16-1) * (1-rgb[0]))
+            self.green.duty_cycle = math.floor((2**16-1) * (1-rgb[1]))
+            self.blue.duty_cycle = math.floor((2**16-1) * (1-rgb[2]))
+    def do_the_color_ramp_dance(self, hue, duration, up_and_down = False, count = 1):
+        if self.is_on:
+            if up_and_down:
+                duration = duration / 2
+            while count > 0:
+                for i in range(0, 100, 1):
+                    # we only go up to 50% lightness because it's white afterwards.
+                    self.set_color_hsl(hue, 1, i/2/100)
+                    time.sleep(duration / 100)
+                if up_and_down:
+                    for i in range(100, 0, -1):
+                        # we only go up to 50% lightness because it's white afterwards.
+                        self.set_color_hsl(hue, 1, i/2/100)
+                        time.sleep(duration / 100)
+                count = count - 1
